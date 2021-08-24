565eb4d08c1e8ba5dd2a2cdc19ee5010504f235a
jailson <jaiilsonaraujo77@gmail.com>
2021-08-23 21:43:43 -0300

    ok

--- script/script.js
+++ script/script.js
@@ -5,3 +5,24 @@ btn.addEventListener("click", function(){
 
     menuLink.classList.toggle("menuL");
 });
+
+// BARRAS DE PROGRESSO DA SEÇÂO CONHECIMENTOS
+var progSecondary1=document.getElementById("progSecondary1");
+var nivelProg1=document.getElementById("nivelProg1");
+
+var progSecondary2=document.getElementById("progSecondary2");
+var nivelProg2=document.getElementById("nivelProg2");
+
+var width=90;
+var tamanho=0;
+
+function animaProg(){
+    if(tamanho>=width){
+        clearInterval(intervalo);
+    }else{
+        tamanho++;
+        progSecondary1.style.width=tamanho+"%";
+        nivelProg1.innerHTML=tamanho+"%";
+    }
+}
+var intervalo=setInterval(animaProg,10);
