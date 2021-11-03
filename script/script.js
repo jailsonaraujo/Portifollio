2f5ca2bc23188d42136250f293e91255a3bf1cc8
jailson <jaiilsonaraujo77@gmail.com>
2021-11-03 19:18:07 -0300

    ok

--- script/script.js
+++ script/script.js
@@ -1,64 +1,73 @@
-var btn=document.getElementById("botao-menu");
-
-btn.addEventListener("click", function(){
-    var menuLink=document.getElementById("menu-link");
-
-    menuLink.classList.toggle("menuL");
+window.addEventListener("scroll", function(){
+    if(window.scrollY>410){
+        setInterval(frame1, 80);
+        setInterval(frame2, 80);
+        setInterval(frame3, 80);
+    }
 });
 
-// BARRAS DE PROGRESSO DA SEÇÂO CONHECIMENTOS
+// javascript **************************************
+var elemento1=document.getElementById("barra1");
+var width1=0;
+var tamanho1=50;
+// var id1=setInterval(frame1, 10);
 
-// animação conhecimentos 1
-var progSecondary1=document.getElementById("progSecondary1");
-var nivelProg1=document.getElementById("nivelProg1");
-
-var width=95;
-var tamanho=0;
-
-function animaProg1(){
-
-    if(tamanho>=width){
-        clearInterval(intervalo1);
+function frame1(){
+    if(width1 < tamanho1){
+        width1++;
+        elemento1.style.width=width1+'%';
+        document.getElementById("painel1").innerHTML=width1 * 1+'%';
     }else{
-        tamanho++;
-        progSecondary1.style.width=tamanho+"%";
-        nivelProg1.innerHTML=tamanho+"%";
+        // clearInterval(id1);
     }
 }
-var intervalo1=setInterval(animaProg1,10);
 
-// animação conhecimentos 2
-var progSecondary2=document.getElementById("progSecondary2");
-var nivelProg2=document.getElementById("nivelProg2");
+// css ***********************************************
+var elemento2=document.getElementById("barra2");
+var width2=0;
+var tamanho2=85;
+// var id2=setInterval(frame2, 20);
 
-var width2=80;
-var tamanho2=0;
-
-function animaProg2(){
-    if(tamanho2>=width2){
-        clearInterval(intervalo2);
+function frame2(){
+    if(width2 < tamanho2){
+        width2++;
+        elemento2.style.width=width2+'%';
+        document.getElementById("painel2").innerHTML=width2 * 1+'%';
     }else{
-        tamanho2++;
-        progSecondary2.style.width=tamanho2+"%";
-        nivelProg2.innerHTML=tamanho2+"%";
+        // clearInterval(id2);
     }
 }
-var intervalo2=setInterval(animaProg2,10);
 
-// animação conhecimentos 3
-var progSecondary3=document.getElementById("progSecondary3");
-var nivelProg3=document.getElementById("nivelProg3");
+// html *******************************************************
+var elemento3=document.getElementById("barra3");
+var width3=0;
+var tamanho3=90;
+// var id3=setInterval(frame3, 30);
 
-var width3=75;
-var tamanho3=0;
-
-function animaProg3(){
-    if(tamanho3>=width3){
-        clearInterval(intervalo3);
+function frame3(){
+    if(width3 < tamanho3){
+        width3++;
+        elemento3.style.width=width3+'%';
+        document.getElementById("painel3").innerHTML=width3 * 1+'%';
     }else{
-        tamanho3++;
-        progSecondary3.style.width=tamanho3+"%";
-        nivelProg3.innerHTML=tamanho3+"%";
+        // clearInterval(id3);
     }
 }
-var intervalo3=setInterval(animaProg3,10);
+
+// animando o dataset automatico com scroll
+const target=document.querySelectorAll('[data-anime]');
+const animaClass="animation";
+
+function animaScroll(){
+    const windowTop=pageYOffset;
+    target.forEach(function(element){
+        if(windowTop > element.offsetTop){
+            element.classList.add(animaClass);
+        }else{
+            element.classList.remove(animaClass);
+        }
+    })
+    
+}
+
+window.addEventListener("scroll", animaScroll);
