9dfb6c79c8da40601082c5106417aefed0885c94
jailson <jaiilsonaraujo77@gmail.com>
2021-11-15 15:23:04 -0300

    atualizado...

--- script/script.js
+++ script/script.js
@@ -14,3 +14,37 @@ function valorProg(qtd){
     }
 }
 
+// animar menu
+const targetMenu=document.querySelectorAll('[data-backGround]');
+const targetAnimaSecao=document.querySelectorAll('[data-anime]');
+
+const animaClassMenu="corFundo";
+const animaClassSecao="aparecer";
+
+function animaScroll(){
+    const windowTopMenu=window.pageYOffset-370;
+    const windowTopSecao=window.pageYOffset + ((innerHeight * 3)/4);
+
+    targetMenu.forEach(function(elementMenu){
+        if(windowTopMenu > elementMenu.offsetTop){
+            elementMenu.classList.add(animaClassMenu);
+        }else{
+            elementMenu.classList.remove(animaClassMenu);
+            elementMenu.style.transition=" all .7s";
+        }
+    });
+
+    targetAnimaSecao.forEach(function(elementSecao){
+        if(windowTopSecao > elementSecao.offsetTop){
+            elementSecao.classList.add(animaClassSecao);
+        }else{
+            elementSecao.classList.remove(animaClassSecao);
+        }
+    });
+    
+}
+
+window.addEventListener('scroll', function (){
+    animaScroll();
+});
+
